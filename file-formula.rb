3196a325d299f51c5ccd03553f3edee626d198ff
JCount <JCount42@gmail.com>
2016-06-26 16:06:09 -0400

    file-formula 5.28
    Closes #626.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- file-formula.rb
+++ file-formula.rb
@@ -2,9 +2,9 @@
 class FileFormula < Formula
   desc "Utility to determine file types"
   homepage "https://www.darwinsys.com/file/"
-  url "ftp://ftp.astron.com/pub/file/file-5.25.tar.gz"
-  mirror "https://fossies.org/linux/misc/file-5.25.tar.gz"
-  sha256 "3735381563f69fb4239470b8c51b876a80425348b8285a7cded8b61d6b890eca"
+  url "ftp://ftp.astron.com/pub/file/file-5.28.tar.gz"
+  mirror "https://fossies.org/linux/misc/file-5.28.tar.gz"
+  sha256 "0ecb5e146b8655d1fa84159a847ee619fc102575205a0ff9c6cc60fc5ee2e012"
 
   head "https://github.com/file/file.git"
 
@@ -21,9 +21,6 @@ class FileFormula < Formula
   depends_on "libmagic"
 
   def install
-    # Clean up "src/magic.h" as per http://bugs.gw.com/view.php?id=330
-    rm "src/magic.h"
-
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make", "install-exec"
